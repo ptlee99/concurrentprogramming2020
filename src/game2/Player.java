fe51c1f8190770896ae53be1479e84311ad54bd7
AhHuei <hueilau@users.noreply.github.com>
2020-06-04 20:21:29 +0800

    GUI window

---
+++ src/game2/Player.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game2;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * @author User
+ */
+public class Player implements Runnable {
+
+    private String threadName;            //the name of the player
+    private Set<Coordinate> pointSet;     //points created
+    private Set<Coordinate> playerPoints = Collections.synchronizedSet(new HashSet<>()); //points taken by the player
+    private static Set<Coordinate> pointsTaken = Collections.synchronizedSet(new HashSet<>()); //points taken by all players
+    private Object pt; //point that is taken
+    private int attempt = 0; //number of attempts
+
+    private final Lock lock = new ReentrantLock();
+
+    public Player(String threadName, Set<Coordinate> pointSet) {
+        this.threadName = threadName;
+        this.pointSet = pointSet;
+    }
+
+    //create the players
+    public void createThread() {
+        Thread thread = new Thread(threadName);
+        thread.start();
+    }
+
+    public void pickPoint() {
+        lock.lock();
+        try {
+
+            //randomly picks 1 point from the set in Points object
+            int size = pointSet.size(); //get the index
+            int p1 = new Random().nextInt(size);
+            int i = 0;
+
+            Iterator setItr = pointSet.iterator();
+            while (setItr.hasNext()) {
+                Object item = new Object();
+                item = setItr.next();
+                if (i == p1) {
+                    pt = item;
+                    if (pointsTaken.add((Coordinate) pt)) {
+                        playerPoints.add((Coordinate) pt); //add point to the player
+                        System.out.println("Point " + pt + " taken by " + threadName);
+                    } else {
+                        attempt++;
+                        System.out.println("Attempt : " + attempt);
+                    }
+                }
+                i++;
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void displayResults() {
+        System.out.println(threadName + " has collected these points: " + playerPoints);
+    }
+
+    @Override
+    public void run() {
+        GameTimer gt = new GameTimer();
+        while (attempt < 20 && !Thread.interrupted()) {
+            if (gt.getIsTimeUp() == false) {
+                System.out.println("SAVE ME");
+                System.out.println("Attempt from run : " + attempt + " by " + Thread.currentThread().getName());
+                pickPoint();
+            }
+
+        }
+        Thread.currentThread().interrupt();
+        displayResults();
+    }
+}
