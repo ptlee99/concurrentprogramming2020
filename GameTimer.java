c34349a214cad5e477e3ba1f48d54f513272c3d5
YShan98 <viviantanyingshan@gmail.com>
2020-06-10 21:09:36 +0800

    Add files via upload

---
+++ GameTimer.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game2;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ *
+ * @author User
+ */
+public class GameTimer{
+    private int m;
+    private Timer timer;
+    private boolean isTimeUp = false;
+
+    public GameTimer(int m){
+        timer = new Timer();
+        timer.schedule(new GameTimeTask(), m*1000);
+    }
+    
+    public GameTimer(){
+    }
+    
+    public boolean getIsTimeUp(){ 
+        return this.isTimeUp;
+    }
+    
+    class GameTimeTask extends TimerTask{
+        @Override
+        public void run() {
+            isTimeUp = true;
+            System.out.println("Time's up!\n"+new Date());
+            timer.cancel();
+        }
+    }
+    
+}
+
+
---
+++ Grid.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game2;
+
+/**
+ *
+ * @author User
+ */
+public class Grid {
+    private final float MAX_X = (float) 1000.00;
+    private final float MAX_Y = (float) 1000.00;
+
+    public float getMAX_X() {
+        return MAX_X;
+    }
+
+    public float getMAX_Y() {
+        return MAX_Y;
+    }
+}
