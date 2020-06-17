7af2398389de329cdceabdbb80b3c2257d6a3a08
YShan98 <viviantanyingshan@gmail.com>
2020-06-17 22:18:18 +0800

    Update Player.java

--- src/game2/Player.java
+++ src/game2/Player.java
@@ -19,12 +19,14 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class Player implements Runnable {
 
-    private String threadName;            //the name of the player
-    private Set<Coordinate> pointSet;     //points created
-    private Set<Coordinate> playerPoints = Collections.synchronizedSet(new HashSet<>()); //points taken by the player
-    private static Set<Coordinate> pointsTaken = Collections.synchronizedSet(new HashSet<>()); //points taken by all players
-    private Object pt; //point that is taken
-    private int attempt = 0; //number of attempts
+    private String threadName; // the name of the player
+    private Set<Coordinate> pointSet; // points created
+    private Set<Coordinate> playerPoints = Collections.synchronizedSet(new HashSet<>()); // points taken by the player
+    private static Set<Coordinate> pointsTaken = Collections.synchronizedSet(new HashSet<>()); // points taken by all
+                                                                                               // players
+    private Set<Edge> edgeSets = Collections.synchronizedSet(new HashSet<>()); // edges created by a player
+    private Object pt; // point that is taken
+    private int attempt = 0; // number of attempts
 
     private final Lock lock = new ReentrantLock();
 
@@ -33,7 +35,7 @@ public class Player implements Runnable {
         this.pointSet = pointSet;
     }
 
-    //create the players
+    // create the players
     public void createThread() {
         Thread thread = new Thread(threadName);
         thread.start();
@@ -43,8 +45,8 @@ public class Player implements Runnable {
         lock.lock();
         try {
 
-            //randomly picks 1 point from the set in Points object
-            int size = pointSet.size(); //get the index
+            // randomly picks 1 point from the set in Points object
+            int size = pointSet.size(); // get the index
             int p1 = new Random().nextInt(size);
             int i = 0;
 
@@ -55,11 +57,11 @@ public class Player implements Runnable {
                 if (i == p1) {
                     pt = item;
                     if (pointsTaken.add((Coordinate) pt)) {
-                        playerPoints.add((Coordinate) pt); //add point to the player
+                        playerPoints.add((Coordinate) pt); // add point to the player
                         System.out.println("Point " + pt + " taken by " + threadName);
                     } else {
                         attempt++;
-                        //System.out.println("Attempt : " + attempt);
+                        // System.out.println("Attempt : " + attempt);
                     }
                 }
                 i++;
@@ -71,8 +73,37 @@ public class Player implements Runnable {
 
     public void displayResults() {
         System.out.println(threadName + " has collected these points: " + playerPoints);
-        //LineWindow line = new LineWindow(playerPoints);
-        
+        System.out.println(threadName + " has created these edges: " + edgeSets);
+        System.out.println(threadName + " has created " + edgeSets.size() + " edges.");
+        // LineWindow line = new LineWindow(playerPoints);
+
+    }
+
+    public void createEdge() {
+        Set<Coordinate> edgePoints = Collections.synchronizedSet(new HashSet<>());
+        playerPoints.forEach(e -> edgePoints.add(e));
+        lock.lock();
+        try {
+            Coordinate p1; // first point
+            Coordinate p2; // second point
+
+            Iterator<Coordinate> pointSet = edgePoints.iterator();
+            while (pointSet.hasNext()) {
+                p1 = pointSet.next();
+                pointSet.remove();
+                if (pointSet.hasNext()) {
+                    p2 = pointSet.next();
+                    pointSet.remove();
+                    edgeSets.add(new Edge(p1, p2));
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int getNumEdge() {
+        return edgeSets.size();
     }
 
     @Override
@@ -80,12 +111,14 @@ public class Player implements Runnable {
         GameTimer gt = new GameTimer();
         while (attempt < 20 && !Thread.interrupted()) {
             if (gt.getIsTimeUp() == false) {
-                //System.out.println("SAVE ME");
-                //System.out.println("Attempt from run : " + attempt + " by " + Thread.currentThread().getName());
+                // System.out.println("SAVE ME");
+                // System.out.println("Attempt from run : " + attempt + " by " +
+                // Thread.currentThread().getName());
                 pickPoint();
             }
 
         }
+        createEdge();
         Thread.currentThread().interrupt();
         displayResults();
     }
