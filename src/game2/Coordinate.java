9679516c5afc26b9597aa9988e4cd105e8498acf
ptlee99 <50142440+ptlee99@users.noreply.github.com>
2020-06-22 16:45:02 +0800

    Update Coordinate with new constructor

--- src/game2/Coordinate.java
+++ src/game2/Coordinate.java
@@ -20,6 +20,8 @@ public class Coordinate {
     private float x;
     private float y;
     
+    public Coordinate(){}
+    
     public Coordinate(float x, float y){
         this.x = x;
         this.y = y;
