76c5637fc06323970bba6bd8934e51743d4cbe7e
ptlee99 <50142440+ptlee99@users.noreply.github.com>
2020-06-22 16:57:33 +0800

    Update Game2 as main GUI

--- src/game2/Game2.java
+++ src/game2/Game2.java
@@ -5,74 +5,47 @@
  */
 package game2;
 
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+
+//gui import
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
  * @author User
  */
-public class Game2 {
+public class Game2 extends Application{
 
-    /**
-     * @param args the command line arguments
-     */
+   Stage window;
+    Scene scene;
+    ArrayList<Integer> numEdge = new ArrayList<>();
+
     public static void main(String[] args) {
-        int n; //number of points
-        int m; //game timer
-        int t; //number of thread
-        
-        //Game start
-        System.out.println("Hi! Ready for the game?");
-        System.out.println("");
-        //input from user, n, m, t
-        Scanner scanner = new Scanner(System.in);
-        
-        //check if n > t
-        do{
-            System.out.println("Enter the number of points : ");
-            n = scanner.nextInt();
-            System.out.println("Enter the number of players: ");
-            t = scanner.nextInt();
-            if(n<t){
-                System.out.println("Number of points should be greater than the number of players.");
-            }
-        }
-        while(n<t);
-        
-        System.out.println("How long you want the game to be? (seconds) ");
-        m = scanner.nextInt();
-        
-        //start timer
-        GameTimer gt = new GameTimer(m);
-        System.out.println("Game Start!"+new Date());
-        
-        //generate random points
-        Points p = new Points();
-        for(int i=0; i<n; i++){
-        p.createPoint();
-        }
-        
-        //get created points
-        Set<Coordinate> pointSet = new HashSet<>();
-        pointSet = p.getSet();
-        
-        //players join the game
-        ExecutorService executor = Executors.newFixedThreadPool(t);
-        
-        for(int i = 1; i <= t; i++){ 
-            Player players = new Player("P" + i, pointSet);
-            players.createThread(); 
-            executor.execute(players);
-            System.out.println("Player " + i + " joins the game.");
-        }
-        executor.shutdown();
+        launch(args);
+
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        window = stage;
+        window.setTitle("NodeGame");
+
+        startGUI start = new startGUI();
+
+        // put the grid into the scene
+        Scene scene1 = new Scene(start.getGrid(), 500, 427);
+
+        // put the scene into the window
+        window.setScene(scene1);
+        scene1.getStylesheets().add(Game2.class.getResource("Game.css").toExternalForm());
+        
+        // show the window
+        window.show();
+
+        
+    }
+
     
 }
